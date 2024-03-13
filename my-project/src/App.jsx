f467823a026268971bd1da6d3fba60f1f43c3a25
jihenhaddaji <jihenhaddaji3012@gmail.com>
2024-03-13 07:12:00 -0800

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Login from './auth/Login'
+
+
+function App() {
+
+
+  return (
+    <div>
+     
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    <Login />
+  
+
+    </div>
+  )
+}
+
+export default App
